eb601c3c197c411c77a93ca1713be7c24f465117
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2021-01-08 21:13:09 -0600

    added remove movie logic

--- script.js
+++ script.js
@@ -9,11 +9,27 @@ $(document).ready(function () {
         console.log("moviedata 1", movieData.Search)
         appendMovies(movieData.Search)
         $(document).on('click', '.nominate-btn', function(){
-            selectedMovie = $(this).attr('id')
+            let selectedMovie = $(this).attr('id')
             let nominatedMovie = nominateMovie(movieData.Search, selectedMovie)
             console.log(nominatedMovie[0])
             moveToNominate(nominatedMovie[0])
         } )
+
+        $(document).on('click', '.remove-btn', function(){
+            let selectedMovieId = $(this).attr('id');
+            $(`#${selectedMovieId}`).remove();
+
+            let removedMovie = nominateMovie(movieData.Search, selectedMovieId);
+            let element = `
+            <div id="${removedMovie[0].imdbID}">
+                <p>${removedMovie[0].Title}</p>
+                <p>${removedMovie[0].Year}</p>
+                <button type="button" class="btn btn-primary nominate-btn" id="${removedMovie[0].imdbID}">Nominate</button>
+            </div>`
+            $('.movies-result-container').prepend(element)
+            // appendElement(element, ".movies-result-container")
+
+        })
     })
 
     const getMovieData = async (movieTitle) => {
@@ -28,9 +44,9 @@ $(document).ready(function () {
         dataArray.forEach(movie => {
             let element = `
             <div id="${movie.imdbID}">
-            <p>${movie.Title}</p>
-            <p>${movie.Year}</p>
-            <button type="button" class="btn btn-primary nominate-btn" id="${movie.imdbID}">Nominate</button>
+                <p>${movie.Title}</p>
+                <p>${movie.Year}</p>
+                <button type="button" class="btn btn-primary nominate-btn" id="${movie.imdbID}">Nominate</button>
             </div>`
             appendElement(element, ".movies-result-container")
         });
@@ -44,17 +60,18 @@ $(document).ready(function () {
         return movieData.filter(movie => movie.imdbID === movieId)
     }
 
-    const removeNominatedMovieFromResults = (imdbID) => {
+    const removeNominatedMovie = (movieData, movieId) => {
+        let movie =  movieData.filter(movie => movie.imdbID === movieId)
 
     }
 
-    const moveToNominate = (movie) =>{
+    const moveToNominate = (movie) => {
 
         let element = `
-        <div>
+        <div id="${movie.imdbID}">
             <p>${movie.Title}</p>
             <p>${movie.Year}</p>
-            <button type="button" class="btn btn-primary nominate-btn" id="${movie.imdbID}">Remove</button>
+            <button type="button" class="btn btn-primary remove-btn" id="${movie.imdbID}">Remove</button>
         </div>`
         // remove from results
         $(`div#${movie.imdbID}`).remove();
